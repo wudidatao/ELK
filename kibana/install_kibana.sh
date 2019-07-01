46e61fa264de4a5a51e30cc56334c61588ce6686
wudidatao <82302925@qq.com>
2019-07-01 22:05:48 +0800

    Update install_kibana.sh

--- kibana/install_kibana.sh
+++ kibana/install_kibana.sh
@@ -8,9 +8,9 @@ rpm -ivh kibana-6.6.0-x86_64.rpm
 cd /etc/kibana/
 
 sed -i "s/#server.port: 5601/server.port: 5601/g" kibana.yml
-sed -i "s/#server.host: "localhost"/server.host: \"$host_ip\"/g" kibana.yml
-sed -i "s/#elasticsearch.hosts: [\"http:\/\/localhost:9200\/elasticsearch.hosts: [\"http:\/\/$host_ip:9200"]//g" kibana.yml
-#sed -i "s///g" kibana.yml
+sed -i "s/#server.host: \"localhost\"/server.host: \"$host_ip\"/g" kibana.yml
+sed -i "s/#elasticsearch.hosts/elasticsearch.hosts/g" kibana.yml
+sed -i "s&http://localhost:9200&http://$host_ip:9200&g" kibana.yml
 
 systemctl start kibana
 systemctl enable kibana 
