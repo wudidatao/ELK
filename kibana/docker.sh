bd4b31a3376816365575e86056a53329d6e7974f
wudidatao <82302925@qq.com>
2019-11-12 17:21:16 +0800

    Update docker.sh

--- kibana/docker.sh
+++ kibana/docker.sh
@@ -1,7 +1,10 @@
+#首次启动，创建容器
 docker-compose up -d
 
 #初始化容器
 docker cp 9414e701e6b2:/usr/share/kibana /data/
+#容器写入时会有权限问题，暂设777解决
+chmod -R 777 /data/kibana
 
-重新加载
+#二次启动，映射配置
 docker-compose up -d
